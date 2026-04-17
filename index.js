510c65e8764465460560ff8465ac3cfbcc5240ed
Ccode-400 <stanleymngige@gmail.com>
2026-04-17 17:20:52 +0300

    added body styles

---
+++ index.js
@@ -0,0 +1,5 @@
+const form = document.getElementById("searchForm");
+const input = document.getElementById("wordInput");
+const resultDiv = document.getElementById("result");
+const errorDiv = document.getElementById("error");
+
