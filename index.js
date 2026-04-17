b407e91638fd03800d85b0c4b26cb4ff4e89ca65
Ccode-400 <stanleymngige@gmail.com>
2026-04-17 17:56:33 +0300

    added form event listener

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const input = document.getElementById("wordInput");
 const resultDiv = document.getElementById("result");
 const errorDiv = document.getElementById("error");
 
+// Event listener
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); 
+  const word = input.value.trim();
 
